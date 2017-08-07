328d09347913413ec9f41268c059d3258f8dce42
Sanich <sancho@cdrpro.ru>
2017-08-07 15:59:38 +0300

    Reformat JS

--- js/app.js
+++ js/app.js
@@ -1,218 +1,224 @@
 (function () {
-    'use strict';
+  'use strict';
+
+  /**
+   * XHR wrapper
+   *
+   * @param {string} url
+   * @param {Function} callback(json, error)
+   */
+  function fetch(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+
+      if (xhr.status !== 200) {
+        callback(null, xhr.status + ': ' + xhr.statusText);
+      } else {
+        callback(JSON.parse(xhr.responseText));
+      }
+    };
+
+    xhr.send();
+  }
+
+
+  /**
+   * Storage
+   *
+   * @returns {Object}
+   * @constructor
+   */
+  function Storage() {
+    var PREFIX = 'cdrpro-v4-';
+
+    var supported = (function () {
+      try {
+        return 'localStorage' in window && window['localStorage'] !== null
+      } catch (e) {
+        return false
+      }
+    })();
 
     /**
-     * XHR wrapper
-     *
-     * @param {string} url
-     * @param {Function} callback(json, error)
+     * Get value from localStorage
+     * @param {string} key
+     * @returns {string|null|undefined}
      */
-    function fetch(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState !== 4) return;
-
-            if (xhr.status !== 200) {
-                callback(null, xhr.status + ': ' + xhr.statusText);
-            } else {
-                callback(JSON.parse(xhr.responseText));
-            }
-        };
-
-        xhr.send();
+    function read(key) {
+      if (!supported) return;
+      return localStorage.getItem(PREFIX + key);
     }
-
 
     /**
-     * Storage
-     *
-     * @returns {Object}
-     * @constructor
+     * Save value to localStorage
+     * @param {string} key
+     * @param {string} value
      */
-    function Storage() {
-        var PREFIX = 'cdrpro-v4-';
-
-        var supported = (function (){
-            try { return 'localStorage' in window && window['localStorage'] !== null } catch (e) { return false }
-        })();
-
-        /**
-         * Get value from localStorage
-         * @param {string} key
-         * @returns {string|null|undefined}
-         */
-        function read(key) {
-            if (!supported) return;
-            return localStorage.getItem(PREFIX + key);
-        }
-
-        /**
-         * Save value to localStorage
-         * @param {string} key
-         * @param {string} value
-         */
-        function save(key, value) {
-            if (!supported) return;
-            localStorage.setItem(PREFIX + key, value);
-        }
-
-        return {
-            read: read,
-            save: save
-        };
+    function save(key, value) {
+      if (!supported) return;
+      localStorage.setItem(PREFIX + key, value);
     }
 
+    return {
+      read: read,
+      save: save
+    };
+  }
+
+
+  /**
+   * NotificationManager
+   *
+   * @returns {Object}
+   * @constructor
+   */
+  function NotificationManager() {
+    var HIDE_DELAY = 5 * 1000; // 5s
+
+    var HIDDEN_CLASS = 'notification__hidden';
+    var LINK_CLASS = 'notification__link';
+    var MANUAL_CLOSE_CLASS = 'js-manual-close';
+
+    var wrapper = document.createElement('div');
+    wrapper.className = 'notification__wrapper';
+    document.body.appendChild(wrapper);
+
+    function onClick(event) {
+      var target = event.target;
+      if (!target.classList.contains(MANUAL_CLOSE_CLASS)) return;
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      hide(target);
+    }
+
+    wrapper.addEventListener('click', onClick, true);
 
     /**
-     * NotificationManager
-     *
-     * @returns {Object}
-     * @constructor
+     * Show a new notification
+     * @param {string} text
+     * @param {Object} options
+     * @param {string} link
      */
-    function NotificationManager() {
-        var HIDE_DELAY = 5 * 1000; // 5s
+    function show(text, options, link) {
+      if (options === undefined) options = {};
+      var notification;
 
-        var HIDDEN_CLASS = 'notification__hidden';
-        var LINK_CLASS = 'notification__link';
-        var MANUAL_CLOSE_CLASS = 'js-manual-close';
+      if (!link) {
+        notification = document.createElement('div');
+      } else {
+        notification = document.createElement('a');
+        notification.setAttribute('href', link);
+      }
 
-        var wrapper = document.createElement('div');
-        wrapper.className = 'notification__wrapper';
-        document.body.appendChild(wrapper);
+      notification.className = ['notification', HIDDEN_CLASS].join('  ');
+      notification.textContent = text;
 
-        function onClick(event) {
-            var target = event.target;
-            if (!target.classList.contains(MANUAL_CLOSE_CLASS)) return;
+      if (options.type) notification.classList.add('notification--' + options.type);
 
-            event.preventDefault();
-            event.stopPropagation();
+      if (!link && options.auto === false) {
+        notification.classList.add(MANUAL_CLOSE_CLASS);
+      } else if (!!link) {
+        notification.classList.add(LINK_CLASS);
+      }
 
-            hide(target);
+      wrapper.appendChild(notification);
+
+      setTimeout(function () {
+        notification.classList.remove(HIDDEN_CLASS);
+
+        if (options.auto !== false) {
+          setTimeout(function () {
+            hide(notification)
+          }, options.delay || HIDE_DELAY);
         }
-
-        wrapper.addEventListener('click', onClick, true);
-
-        /**
-         * Show a new notification
-         * @param {string} text
-         * @param {Object} options
-         * @param {string} link
-         */
-        function show(text, options, link) {
-            if (options === undefined) options = {};
-            var notification;
-
-            if (!link) {
-                notification = document.createElement('div');
-            } else {
-                notification = document.createElement('a');
-                notification.setAttribute('href', link);
-            }
-
-            notification.className = ['notification', HIDDEN_CLASS].join('  ');
-            notification.textContent = text;
-
-            if (options.type) notification.classList.add('notification--' + options.type);
-
-            if (!link && options.auto === false) {
-                notification.classList.add(MANUAL_CLOSE_CLASS);
-            } else if (!!link) {
-                notification.classList.add(LINK_CLASS);
-            }
-
-            wrapper.appendChild(notification);
-
-            setTimeout(function() {
-                notification.classList.remove(HIDDEN_CLASS);
-
-                if (options.auto !== false) {
-                    setTimeout(function() { hide(notification) }, options.delay || HIDE_DELAY);
-                }
-            }, 100);
-        }
-
-        /**
-         * Hide specific notification
-         * @param {Element} notification
-         */
-        function hide(notification) {
-            notification.classList.add(HIDDEN_CLASS);
-
-            setTimeout(function() {
-                wrapper.removeChild(notification);
-            }, 600);
-        }
-
-        return {
-            show: show
-        }
+      }, 100);
     }
-
 
     /**
-     * Application
-     *
-     * @returns {Object}
-     * @constructor
+     * Hide specific notification
+     * @param {Element} notification
      */
-    function App() {
-        var language = document.documentElement.lang || 'ru';
+    function hide(notification) {
+      notification.classList.add(HIDDEN_CLASS);
 
-        var storage = new Storage();
-        var notificationManager = new NotificationManager();
+      setTimeout(function () {
+        wrapper.removeChild(notification);
+      }, 600);
+    }
 
-        function showNotifications() {
-            function func() {
-                function onFetchResolve(notifications, error) {
-                    if (!Array.isArray(notifications) || error) {
-                        console.error(error);
-                        return false;
-                    }
+    return {
+      show: show
+    }
+  }
 
-                    for (var i = 0, length = notifications.length; i < length; i++) {
-                        var notify = notifications[i];
 
-                        if (!notify.active) continue;
-                        if (storage.read(notify.key)) continue;
+  /**
+   * Application
+   *
+   * @returns {Object}
+   * @constructor
+   */
+  function App() {
+    var language = document.documentElement.lang || 'ru';
 
-                        if (notify.msg[language]) {
-                            notificationManager.show(notify.msg[language], { auto: false }, notify.link);
-                            storage.save(notify.key, true);
-                        }
-                    }
-                }
+    var storage = new Storage();
+    var notificationManager = new NotificationManager();
 
-                fetch('/assets/notifications.json', onFetchResolve);
+    function showNotifications() {
+      function func() {
+        function onFetchResolve(notifications, error) {
+          if (!Array.isArray(notifications) || error) {
+            console.error(error);
+            return false;
+          }
+
+          for (var i = 0, length = notifications.length; i < length; i++) {
+            var notify = notifications[i];
+
+            if (!notify.active) continue;
+            if (storage.read(notify.key)) continue;
+
+            if (notify.msg[language]) {
+              notificationManager.show(notify.msg[language], {auto: false}, notify.link);
+              storage.save(notify.key, true);
             }
-
-            setTimeout(func, 1000);
+          }
         }
 
-        return {
-            showNotifications: showNotifications
-        };
+        fetch('/assets/notifications.json', onFetchResolve);
+      }
+
+      setTimeout(func, 1000);
     }
 
+    return {
+      showNotifications: showNotifications
+    };
+  }
 
-    /** Create and run the App */
 
-    function onContentLoaded() {
-        function run() {
-            var app = new App();
-            app.showNotifications();
-        }
+  /** Create and run the App */
 
-        setTimeout(run, 100);
+  function onContentLoaded() {
+    function run() {
+      var app = new App();
+      app.showNotifications();
     }
 
-    if (document.readyState !== 'loading') {
-        onContentLoaded();
-    } else {
-        document.addEventListener('DOMContentLoaded', onContentLoaded, true);
-    }
+    setTimeout(run, 100);
+  }
 
-    console.log('Hey there! 😎 Wanna see the source code? \nIt\'s here https://github.com/cdrpro/cdrpro.github.io');
+  if (document.readyState !== 'loading') {
+    onContentLoaded();
+  } else {
+    document.addEventListener('DOMContentLoaded', onContentLoaded, true);
+  }
+
+  console.log('Hey there! 😎 Wanna see the source code? \nIt\'s here https://github.com/cdrpro/cdrpro.github.io');
 
 })();
